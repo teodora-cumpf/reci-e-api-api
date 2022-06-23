16231b9aac00e85732065fe86d41e37f160bdcd8
Teodora Cumpf <teodora.cumpf@marbleit.rs>
2022-06-23 13:04:15 +0200

    Fixed build failed.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,4 +1,4 @@
-""""
+"""
 Tests for models
 """
 from django.test import TestCase
@@ -6,7 +6,7 @@ from django.contrib.auth import get_user_model
 
 
 class ModelTests(TestCase):
-    """"Test models."""
+    """Test models."""
 
     def test_create_user_with_email_successful(self):
         """Test creating a user is successful."""
