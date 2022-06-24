2a3d6dedc8da7035fa3b175c94711467382f4ae8
teodora.cumpf <teodora.cumpf@marbleit.rs>
2022-06-24 11:41:03 +0200

    Started creating tests for user.

---
+++ app/user/tests/test_user_api.py
@@ -0,0 +1,19 @@
+"""
+Test for the user API.
+"""
+from django.test imprt TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+
+CREATE_USER_URL = reverse('user:create')
+
+
+def create_user(**params):
+    """Create and return a new user"""
+    return get_user_model().objects.create_user(**params)
+
+
