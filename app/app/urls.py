7b85d85b39a3345246a082e29c93580b09f79143
Teodora Cumpf <teodora.cumpf@marbleit.rs>
2022-06-28 15:06:03 +0200

    Token authentication

--- app/app/urls.py
+++ app/app/urls.py
@@ -20,14 +20,16 @@ from drf_spectacular.views import (
 
 from django.contrib import admin
 from django.urls import path, include
+# import user.urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/user/', include('user.urls')),
+    # path('user/', include(user.urls)),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     path(
         'api/docs/',
         SpectacularSwaggerView.as_view(url_name='api-schema'),
         name='api-docs',
     ),
-    path('api/user/', include('user.urls')),
 ]
--- app/user/views.py
+++ app/user/views.py
@@ -1,7 +1,10 @@
 """
 Views for the user API
 """
+from app.user.serializers import AuthTokenSerializer
 from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from user.serializers import UserSerializer
 
@@ -9,3 +12,9 @@ from user.serializers import UserSerializer
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system"""
     serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for user"""
+    serializer_class = AuthTokenSerializer
+    render_classes = api_settings.DEFAULT_RENDER_CLASSES
